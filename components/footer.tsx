cd1b01df69496cc07e4cbe603a5bd7bd630748f2
DabAZ <dabaz@dabaz.me>
2024-05-26 01:02:31 +0800

    feat: update footer links

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,4 @@
-import { footerNavItems, headerNavItems } from "lib/nav-items"
+import { footerNavItems } from "lib/nav-items"
 
 function ArrowIcon() {
   return (
@@ -20,7 +20,7 @@ function ArrowIcon() {
 export default function Footer() {
   return (
     <footer className="mb-16">
-      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:grid md:grid-cols-4 md:space-y-0 dark:text-neutral-300">
         {Object.entries(footerNavItems).map(([path, { name }]) => {
           return (
             <a
@@ -39,7 +39,7 @@ export default function Footer() {
         })}
       </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        © {new Date().getFullYear()} MIT Licensed
+        © {new Date().getFullYear()} DabAZ
       </p>
     </footer>
   )
--- lib/nav-items.ts
+++ lib/nav-items.ts
@@ -11,16 +11,28 @@ export const headerNavItems = {
 };
 
 export const footerNavItems = {
-  '/': {
-    name: 'Home',
+  'https://x.com/dabaz_official': {
+    name: 'Twitter',
   },
-  '/blog': {
-    name: 'Blog',
+  'https://instagram.com/dabaz_official': {
+    name: 'Instagram',
   },
-  '/contact': {
-    name: 'Contact',
+  'https://github.com/dabaz-official': {
+    name: 'Github',
   },
-  '/about': {
-    name: 'About',
+  'https://open.spotify.com/artist/1KmtWPNNBJw4spLrQkPrXQ?si=LkyJ1g3qQ8SVnrV07hN2pA': {
+    name: 'Spotify',
+  },
+  'mailto:dabaz@dabaz.me': {
+    name: 'Email',
+  },
+  'https://tiktok.com/@dabaz_official': {
+    name: 'TikTok',
+  },
+  'https://www.douyin.com/user/MS4wLjABAAAA8WCQGYJba6IcbQN_VVyupLj8X8Gs19srHzb27cCwuCU': {
+    name: 'Douyin',
+  },
+  'https://space.bilibili.com/316672801': {
+    name: 'Bilibili',
   },
 };
