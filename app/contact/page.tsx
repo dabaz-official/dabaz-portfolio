142fbd1843e8cba0046b42de11a9bf095a9ea7e2
DabAZ <dabaz@dabaz.me>
2024-05-27 02:37:28 +0800

    refactor: update titles

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Contact Me</h1>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Contact</h1>
       <p className="mb-8">
         {`Let's create something great together.`}
       </p>
