ff25da88255b55d87f850ca5de9c7c39aad25358
DabAZ <dabaz@dabaz.me>
2024-05-27 01:31:50 +0800

    chore: update image style

--- components/crafts.tsx
+++ components/crafts.tsx
@@ -8,18 +8,19 @@ export function Crafts() {
     <div>
       {Object.entries(craftsItems).map(([path, { name, imageSrc }]) => {
         return (
-          <Link
+          <a
             key={path}
             className="flex flex-col space-y-1 mb-4"
             href={path}
+            target='_blank'
           >
             <div className="w-full flex flex-col space-y-4">
               <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
                 {name}
               </p>
-              <Image src={imageSrc} alt={name} width={1920} height={1080} />
+              <Image src={imageSrc} alt={name} width={1920} height={1080} className='rounded-xl' />
             </div>
-          </Link>
+          </a>
         )
       }
     )}
