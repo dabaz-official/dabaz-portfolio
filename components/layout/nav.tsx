952253140f05e88f29a61cb92ae55a17e120a118
DabAZ <dabaz@dabaz.me>
2024-07-21 00:24:04 +0800

    move files

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from 'react'
 import { RxHamburgerMenu } from 'react-icons/rx';
 
 import { headerNavItems } from 'lib/nav-items'
+import { Logo } from 'components/common/logo';
 
 export function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -30,7 +31,7 @@ export function Navbar() {
 
   return (
     <header className={`fixed top-0 left-0 right-0 transition-colors duration-200 ${
-      isScrolled ? 'bg-white/80 backdrop-blur' : 'bg-white'
+      isScrolled ? 'bg-white/80 dark:bg-black/60 backdrop-blur' : 'bg-white dark:bg-black'
     }`}>
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-8">
         <div className="flex flex-1">
@@ -38,20 +39,14 @@ export function Navbar() {
             href="/"
             className="-m-1.5 p-1.5"
           >
-            <Image
-              className="h-7 w-auto"
-              src="/logo-black.svg"
-              alt="Logo"
-              width={32}
-              height={32}
-            />
+            <Logo className="h-6 w-auto text-black dark:text-white" />
           </Link>
         </div>
         <div className="flex lg:hidden">
           <button
             type="button"
             onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-neutral-700"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-neutral-700 dark:text-neutral-300"
           >
             <RxHamburgerMenu
               className="h-6 w-6"
@@ -65,7 +60,7 @@ export function Navbar() {
                 <Link
                   key={path}
                   href={path}
-                  className="flex items-center text-sm leading-6 text-neutral-700 transition-all hover:text-neutral-950 hover:underline"
+                  className="flex items-center text-sm leading-6 text-neutral-700 dark:text-neutral-300 transition-all hover:text-neutral-950 dark:hover:text-neutral-50 hover:underline"
                 >
                   {name}
                 </Link>
