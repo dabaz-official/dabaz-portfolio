a7666d0d3804b1c9c056620497370150d9f6913e
DabAZ <dabaz@dabaz.me>
2024-07-21 00:04:31 +0800

    update pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,45 +5,20 @@ import { GeistMono } from 'geist/font/mono'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 
-import { Navbar } from 'components/nav'
-import Footer from 'components/footer'
 import { baseUrl } from 'app/sitemap'
 import { seo } from 'lib/seo'
-import 'styles/global.css'
+import './globals.css'
+import { Navbar } from 'components/nav'
 
 export const metadata: Metadata = {
-  metadataBase: new URL(baseUrl),
+  metadataBase: new URL('https://dabaz.me'),
   title: {
     template: "%s | DabAZ",
     default: seo.home.title,
   },
   description: seo.home.description,
 
-  icons: {
-    icon: "/favicon/favicon.ico",
-    shortcut: "/favicon/favicon.ico",
-    apple: "/favicon/apple-touch-icon.png",
-  },
-
-  referrer: "origin-when-cross-origin",
-  keywords: [
-    "personal website",
-    "technology",
-    "entrepreneurship",
-    "innovation",
-    "networking",
-    "community",
-    "artificial intelligence",
-    "3D printing",
-    "software development",
-    "startup tips",
-    "music production",
-    "rapper",
-    "front-end development",
-    "DabAZ",
-    "Diebold Dai",
-    "dabaz_official",
-  ],
+  keywords: "DabAZ, Diebold Dai, dabaz_official",
   authors: [{ name: "DabAZ", url: "https://dabaz.me" }],
   formatDetection: {
     email: true,
@@ -62,11 +37,9 @@ export const metadata: Metadata = {
   robots: {
     index: true,
     follow: true,
-    nocache: false,
     googleBot: {
       index: true,
       follow: true,
-      noimageindex: false,
       'max-video-preview': 15,
       'max-image-preview': 'large',
       'max-snippet': -1,
@@ -75,9 +48,6 @@ export const metadata: Metadata = {
   twitter: {
     card: "summary_large_image",
     creator: "@dabaz_official",
-    title: seo.home.title,
-    description: seo.home.description,
-    images: seo.home.imageSrc,
   },
 }
 
@@ -92,16 +62,15 @@ export default function RootLayout({
     <html
       lang="en"
       className={cx(
-        'text-black bg-white dark:text-white dark:bg-black',
+        'text-black bg-white h-full antialiased select-none',
         GeistSans.variable,
         GeistMono.variable
       )}
     >
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-          <Navbar />
+      <body>
+        <Navbar />
+        <main className="max-w-7xl p-4 lg:px-8 mx-auto mt-16">
           {children}
-          <Footer />
           <Analytics />
           <SpeedInsights />
         </main>
--- app/page.tsx
+++ app/page.tsx
@@ -4,24 +4,7 @@ import { BlogPosts } from 'components/posts'
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Hello, I'm DabAZ 👋
-      </h1>
-      <p className="mb-8">
-        {`I'm a versatile professional with a passion for cybersecurity, front-end development, and creative arts. As a ethical hacker, entrepreneur, musician, designer, and front-end development enthusiast, I thrive on exploring new technologies and pushing creative boundaries. Whether I'm securing digital landscapes, crafting user-friendly interfaces, or producing music, I bring dedication and innovation to everything I do. Let's connect and create something amazing together!`}
-      </p>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Crafts
-      </h1>
-      <div className="my-8">
-        <Crafts />
-      </div>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        From my blog
-      </h1>
-      <div className="my-8">
-        <BlogPosts />
-      </div>
+      
     </section>
   )
 }
