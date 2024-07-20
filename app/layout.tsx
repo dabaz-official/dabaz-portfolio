ae94ddc5eb3f89c9abbcfa92278b15459f75f853
DabAZ <dabaz@dabaz.me>
2024-07-21 03:19:41 +0800

    update viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import type { Viewport } from 'next'
 
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
@@ -54,6 +55,17 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     creator: "@dabaz_official",
   },
+}
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 }
 
 const cx = (...classes) => classes.filter(Boolean).join(' ')
