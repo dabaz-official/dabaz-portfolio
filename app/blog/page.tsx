93c92b7020a979e10d0d5590f98fe8ca5bdff610
DabAZ <dabaz@dabaz.me>
2024-07-21 07:52:40 +0800

    update blog page and contact page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -8,14 +8,12 @@ export const metadata = {
 export default function Page() {
   return (
     <section className="relative isolate z-10 overflow-hidden pt-8 mx-auto space-y-6 max-w-2xl lg:max-w-none">
-      <h1 className="max-w-2xl text-6xl font-bold tracking-tight sm:text-9xl lg:col-span-2 xl:col-auto">
-        DabBlog
+      <h1 className="text-4xl font-bold tracking-tight text-neutral-900 dark:text-neutral-100 sm:text-6xl">
+        {`DabBlog`}
       </h1>
-      <div className="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1 space-y-4 sm:space-y-6">
-        <h2 className="max-w-2xl text-4xl font-bold tracking-tight text-neutral-600 dark:text-neutral-400 sm:text-6xl lg:col-span-2 xl:col-auto mt-6">
-          I write about my experiences, my craft, and the world around me.
-        </h2>
-      </div>
+      <h2 className="text-2xl font-semibold tracking-tight text-neutral-600 dark:text-neutral-400 sm:text-3xl">
+        {`I write about my experiences, my craft, and the world around me.`}
+      </h2>
       <BlogPosts />
     </section>
   )
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,11 +7,13 @@ export const metadata = {
 
 export default function Page() {
   return (
-    <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tight">Contact</h1>
-      <p className="mb-8">
+    <section className="relative isolate z-10 overflow-hidden pt-8 mx-auto space-y-6 max-w-2xl lg:max-w-none">
+      <h1 className="text-4xl font-bold tracking-tight text-neutral-900 dark:text-neutral-100 sm:text-6xl">
+        Contact
+      </h1>
+      <h2 className="text-2xl font-semibold tracking-tight text-neutral-600 dark:text-neutral-400 sm:text-3xl">
         {`Let's create something great together.`}
-      </p>
+      </h2>
       <ContactForm />
     </section>
   )
