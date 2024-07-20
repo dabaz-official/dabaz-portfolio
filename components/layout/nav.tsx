ce785219d8e9635d202f4bb139e95b6129a6037e
DabAZ <dabaz@dabaz.me>
2024-07-21 00:58:45 +0800

    update navbar

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -1,9 +1,19 @@
 'use client'
 
 import Link from 'next/link'
-import Image from 'next/image';
-import { useEffect, useState } from 'react'
-import { RxHamburgerMenu } from 'react-icons/rx';
+import {
+  useCallback,
+  useEffect,
+  useState
+} from 'react'
+import {
+  RxCross1,
+  RxHamburgerMenu
+} from 'react-icons/rx';
+import {
+  Dialog,
+  DialogPanel
+} from '@headlessui/react';
 
 import { headerNavItems } from 'lib/nav-items'
 import { Logo } from 'components/common/logo';
@@ -27,7 +37,13 @@ export function Navbar() {
     };
   }, []);
 
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const handleLinkClick = useCallback(() => {
+    setTimeout(() => {
+      setMobileMenuOpen(false);
+    }, 200);
+  }, []);
 
   return (
     <header className={`fixed top-0 left-0 right-0 transition-colors duration-200 ${
@@ -46,7 +62,7 @@ export function Navbar() {
           <button
             type="button"
             onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-neutral-700 dark:text-neutral-300"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-neutral-700 dark:text-neutral-300 cursor-pointer"
           >
             <RxHamburgerMenu
               className="h-6 w-6"
@@ -69,6 +85,54 @@ export function Navbar() {
           </div>
         </div>
       </nav>
+
+      {/* Mobile menu */}
+      <Dialog
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+        className="lg:hidden"
+      >
+        <div className="fixed inset-0 z-10" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-40 w-full overflow-y-auto bg-white dark:bg-black p-4 lg:px-8 sm:max-w-sm sm:ring-1 sm:ring-neutral-900/10 dark:sm:ring-neutral-100/10">
+          <div className="flex items-center justify-between">
+            <Link
+              href="/"
+              className="-m-1.5 p-1.5"
+              onClick={handleLinkClick}
+            >
+              <Logo className="h-6 w-auto text-black dark:text-white" />
+            </Link>
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(false)}
+              className="-m-2.5 rounded-md p-2.5 text-neutral-700 dark:text-neutral-300 cursor-pointer"
+            >
+              <RxCross1
+                className="h-6 w-6"
+                fill="none"
+              />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-neutral-500/10 dark:divide-neutral-200">
+              <div className="py-6 space-y-2">
+                {Object.entries(headerNavItems).map(([path, { name }]) => {
+                  return (
+                    <Link
+                      key={path}
+                      href={path}
+                      className="-mx-3 block rounded-lg py-2 px-3 text-xl leading-9 text-neutral-950 dark:text-neutral-50 font-semibold hover:bg-neutral-50 dark:hover:bg-neutral-900"
+                      onClick={handleLinkClick}
+                    >
+                      {name}
+                    </Link>
+                  )
+                })}
+              </div>
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
     </header>
   )
 };
