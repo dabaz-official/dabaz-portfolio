6e27c162ef3b4ef6a3e80acfbab7c1bed368f21f
DabAZ <dabaz@dabaz.me>
2024-07-21 07:24:42 +0800

    update social links

--- lib/nav-items.ts
+++ lib/nav-items.ts
@@ -9,33 +9,3 @@ export const headerNavItems = {
     name: 'Contact',
   }
 };
-
-export const footerNavItems = {
-  'https://x.com/dabaz_official': {
-    name: 'Twitter',
-  },
-  'https://instagram.com/dabaz_official': {
-    name: 'Instagram',
-  },
-  'https://github.com/dabaz-official': {
-    name: 'Github',
-  },
-  'https://open.spotify.com/artist/1KmtWPNNBJw4spLrQkPrXQ?si=LkyJ1g3qQ8SVnrV07hN2pA': {
-    name: 'Spotify',
-  },
-  'mailto:dabaz@dabaz.me': {
-    name: 'Email',
-  },
-  'https://youtube.com/@dabaz_official': {
-    name: 'YouTube',
-  },
-  'https://tiktok.com/@dabaz_official': {
-    name: 'TikTok',
-  },
-  'https://www.douyin.com/user/MS4wLjABAAAA8WCQGYJba6IcbQN_VVyupLj8X8Gs19srHzb27cCwuCU': {
-    name: 'Douyin',
-  },
-  'https://space.bilibili.com/316672801': {
-    name: 'Bilibili',
-  },
-};
